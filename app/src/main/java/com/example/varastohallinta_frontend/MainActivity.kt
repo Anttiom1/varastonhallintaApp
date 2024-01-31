56e93a820289e8ef81989937d7cf60f41d2af410
Anttiom1 <antti-mursu@hotmail.com>
2024-01-31 13:58:41 +0200

    Added username, password textfields and login button

--- app/src/main/java/com/example/varastohallinta_frontend/MainActivity.kt
+++ app/src/main/java/com/example/varastohallinta_frontend/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,8 +32,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greetings("Juuh")
-                    //testing
+                    Column(
+                        //Column täyttää koko ruudun
+                        modifier = Modifier.fillMaxSize(),
+                        //Keskittää pystysuuntaan
+                        verticalArrangement = Arrangement.Center,
+                        //Keskittää vaakasuuntaan
+                        horizontalAlignment = Alignment.CenterHorizontally
+
+                    ){
+                        OutlinedTextField(value = "",
+                            onValueChange = {},
+                            placeholder = {Text("Username")}
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        OutlinedTextField(value = "",
+                            onValueChange = {},
+                            placeholder = {Text("Password")}
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = { }) {
+                            Text("Login")
+                            
+                        }
+
+                    }
                 }
             }
         }
