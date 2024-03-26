ffca6ecd4c38a8d5b080bc5db1f396b3cf25e6ce
Antti Mursu <Antti.Mursu@edu.lapinamk.fi>
2024-03-26 22:12:57 +0200

    Update RentalItemAddViewModel.kt

--- app/src/main/java/com/example/varastohallinta_frontend/viewmodel/RentalItemAddViewModel.kt
+++ app/src/main/java/com/example/varastohallinta_frontend/viewmodel/RentalItemAddViewModel.kt
@@ -6,15 +6,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.varastohallinta_frontend.AccountDatabase
+import com.example.varastohallinta_frontend.DbProvider
+import com.example.varastohallinta_frontend.DbProvider.db
+import com.example.varastohallinta_frontend.api.authService
 import com.example.varastohallinta_frontend.api.rentalItemsServices
 import com.example.varastohallinta_frontend.model.AddRentalItemReq
+import com.example.varastohallinta_frontend.model.LoggedUserRes
 import com.example.varastohallinta_frontend.model.RentalItemState
 import kotlinx.coroutines.launch
 
-class RentalItemAddViewModel(savedStateHandle: SavedStateHandle): ViewModel() {
+class RentalItemAddViewModel(
+    savedStateHandle: SavedStateHandle,): ViewModel() {
+
     val categoryId = savedStateHandle.get<String>("categoryId")?.toIntOrNull() ?: 0
     private val _rentalItemState = mutableStateOf(RentalItemState())
     val rentalItemState : State<RentalItemState> = _rentalItemState
+    private var id: Int = 0
 
     fun setDone(done: Boolean){
         _rentalItemState.value = _rentalItemState.value.copy(done = done)
@@ -27,8 +35,15 @@ class RentalItemAddViewModel(savedStateHandle: SavedStateHandle): ViewModel() {
     fun addItemToCategory(){
         viewModelScope.launch {
             try {
+                val accessToken = db.accountDao().getToken()
+                accessToken?.let {
+                    val loggedUser = authService.getAccount("bearer $it")
+                    id = loggedUser.authUserId
+                }
                 _rentalItemState.value = _rentalItemState.value.copy(loading = true)
-                rentalItemsServices.addItemToCategory(categoryId, AddRentalItemReq(rentalItemName = _rentalItemState.value.rentalItemName))
+                rentalItemsServices.addItemToCategory(categoryId, AddRentalItemReq(
+                    rentalItemName = _rentalItemState.value.rentalItemName,
+                    createdByUserId = id))
                 setDone(true)
             }
             catch (e: Exception){
